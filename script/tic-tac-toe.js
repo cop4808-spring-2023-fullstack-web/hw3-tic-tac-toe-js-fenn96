0f75f4438375759166e804723cd119cd0d1c052d
Christian Fenn <cfenn2020@fau.edu>
2023-02-12 07:42:28 -0500

    Restart with proper starting repo

--- script/tic-tac-toe.js
+++ script/tic-tac-toe.js
@@ -1,4 +1,4 @@
-const statusDisplay = document.querySelector('.game--status');
+const statusDisplay = document.querySelector('.status');
 
 let gameActive = true;
 let currentPlayer = "X";
@@ -33,29 +33,32 @@ function handlePlayerChange() {
 
 function handleResultValidation() {
     let roundWon = false;
-    for(let i = 0; i <= 7; i++) {
+    for (let i = 0; i <= 7; i++) {
         const winCondition = winningConditions[i];
-        const a = gameState[winCondition[0]];
-        const b = gameState[winCondition[1]];
-        const c = gameState[winCondition[2]];
-        if(a === '' || b === '' || c === '')
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
             continue;
-        if(a === b && b === c) {
+        }
+        if (a === b && b === c) {
             roundWon = true;
             break
         }
     }
 
-    if(roundWon) {
+    if (roundWon) {
         statusDisplay.innerHTML = winningMessage();
         gameActive = false;
+        statusDisplay.style.color = "rgb(251,100,204)";
         return;
     }
 
-    const roundDraw = !gameState.includes("");
-    if(roundDraw) {
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
         statusDisplay.innerHTML = drawMessage();
         gameActive = false;
+        statusDisplay.style.color = "rgb(251,100,204)";
         return;
     }
 
@@ -66,8 +69,9 @@ function handleCellClick(clickedCellEvent) {
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
-    if(gameState[clickedCellIndex] !== "" || !gameActive)
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
         return;
+    }
 
     handleCellPlayed(clickedCell, clickedCellIndex);
     handleResultValidation();
@@ -77,10 +81,10 @@ function handleRestartGame() {
     gameActive = true;
     currentPlayer = "X";
     gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.style.color = "rgb(65, 65, 65)";
     statusDisplay.innerHTML = currentPlayerTurn();
     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
 }
 
-
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+document.querySelector('.restart').addEventListener('click', handleRestartGame);
